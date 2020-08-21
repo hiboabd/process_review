0c64de0f000d24e30c2b5f2d26c3c61194bb1a03
hiboabd <51047911+hiboabd@users.noreply.github.com>
2020-08-21 10:26:56 +0100

    Failing test: function should evaluate string and return sum in array

--- newProject/spec/CalculateFromStringSpec.js
+++ newProject/spec/CalculateFromStringSpec.js
@@ -2,4 +2,8 @@ describe('CalculateFromString', () => {
   it('Returns ["1", 1] when "1" is inputted', () => {
     expect(calculateFromString("1")).toEqual(["1", 1])
   })
+
+  it('returns ["1 + 2", 3] when "1 + 2" is inputted', () => {
+    expect(calculateFromString("1 + 2")).toEqual(["1 + 2", 3])
+  })
 })
